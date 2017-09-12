388cccde50c1ad5a02696849f0a04d7108d5e214
Aris Santas <asantas@cybraics.com>
2017-09-12 01:57:22 -0400

    Current Website

---
+++ js/aboutController.js
@@ -0,0 +1,14 @@
+function aboutController($scope) {
+  $scope.therapists = [
+    {name: 'Patricia Santas', img: 'img/therapist/patricia.jpg', edu: 'BA, LMT'},
+    {name: 'Aristotelis Santas', img: 'img/therapist/ari.jpg', edu: 'PhD, LMT'},
+    {name: 'Alfreda Cochran', img: 'img/therapist/freda.jpg', edu: 'LMT'},
+    {name: 'Lee McKinney', img: 'img/therapist/lee.jpg', edu: 'LMT'},
+    {name: 'Stephen O\'Haire', img: 'img/therapist/stephen.jpg', edu: 'BA, LMT'},
+    {name: 'Misty Humphries', img: 'img/therapist/misty.jpg', edu: 'LMT'},
+    {name: 'Alethea Santas', img: 'img/therapist/alethea.jpg', edu: 'BA, LMT'},
+    {name: 'Allyse Hargrove', img: 'img/therapist/allyse.jpg', edu: 'AS, LMT'},
+    {name: 'April Milton', img: 'img/therapist/april.jpg', edu: 'LMT'},
+    {name: 'Iain Searcy', img: 'img/therapist/iain.jpg', edu: 'LMT'},
+  ]
+}
---
+++ js/serviceController.js
@@ -0,0 +1,55 @@
+function serviceController($scope) {
+  $scope.services = [
+    {
+      name: 'Swedish Massage',
+      desc: 'Combines effleurage, kneading, and friction on the surface of the muscles with assisted movements of the joints. It relaxes the mind and body, warms up and flushes metabolic waste from the muscles, and improves circulation and range of motion.',
+    },
+    {
+      name: 'Deep Tissue Massage',
+      desc: 'Addresses the deeper layers of the tissues by using slow strokes and deep hand pressure on tight areas. It releases patterns of tension in the muscles and restores suppleness and strength.',
+    },
+    {
+      name: 'Prenatal Massage',
+      desc: 'Addresses muscular tension associated with the changes in the body during the second and third trimester of pregnancy.',
+    },
+    {
+      name: 'Sports Massage',
+      desc: 'For muscle discomfort associated with hard workouts, this technique benefits muscles by warming and softening tissue, realigning muscle fibers, helping to heal scar tissue, and flushing metabolic waste from specific muscles.',
+    },
+    {
+      name: 'Hot Stone Therapy',
+      desc: 'Melts away tension, eases muscle stiffness and increases circulation and metabolism. A hot stone massage therapy session promotes deeper muscle relaxation through the placement of smooth, water-heated stones at key points on the body',
+    },
+    {
+      name: 'Ashiatsu (AOBT)',
+      desc: 'This barefoot technique provides a deep blanket of pressure that penetrates even the most inward muscle groups. It is powerfully relaxing to both body and mind, useful in stimulating the lymphatic system, and effective in treating chronic muscle tension.',
+    },
+    {
+      name: 'CORE Mysofascial Therapy',
+      desc: 'Utilizes deep and systematic strokes to spread and stretch connective tissue, or fascia, in which stress, adhesions and painful tension are held.',
+    },
+    {
+      name: 'Thai Assisted Yoga Massage',
+      desc: 'Utilizes slow and deep compressions and stretches to improve posture, relieve myofascial pain, and restore balance and strength. The work is done on the floor on a mat with the client wearing loose clothing.',
+    },
+    {
+      name: 'Neuromuscular Therapy',
+      desc: 'Neuromuscular Therapy (NMT) is a modern, logical system of massage which utilizes static pressure on specific myofascial points to relieve painful areas. This technique manipulates the soft tissue to bring balance to the body and nervous system.',
+      link: 'https://nmtcenter.com/description/',
+    },
+    {
+      name: 'TMJ Massage',
+      desc: 'Applied to treat dysfunctions associated with temperomandibular joint disorders. Massage techniques are administered externally and intraorally. Intraoral massage is done with gloved hands by a therapist trained in this technique',
+      link: 'http://www.massagetherapyschoolsinformation.com/tmj-dysfunction-mouth-massage/',
+    },
+    {
+      name: 'Lomilomi Massage',
+      desc: 'Lomilomi massage is part of traditional Native Hawaiian medicine. It uses long, flowing, dance-like strokes, gentle stretches and joint rotations. Generous amounts of oils are applied. Traditional lomi lomi may include abdominal massage because Hawaiian tradition considers the colon part of a person\'s soul or heart.',
+      link: 'http://health.howstuffworks.com/wellness/spa-health/lomi-lomi-massage.htm',
+    },
+    {
+      name: 'Body Exfoliation',
+      desc: 'Salt glow treatment uses sea salts combined with oils to exfoliate your skin. By removing old cells, your skin has a vibrant appearance and is smooth and hydrated.',
+    },
+  ]
+}
