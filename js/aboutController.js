522d9d98e9487452c366d1b625100d42829aebd5
Aris Santas <asantas@cybraics.com>
2018-01-13 14:16:53 -0500

    Remove Misty

--- js/aboutController.js
+++ js/aboutController.js
@@ -5,7 +5,6 @@ function aboutController($scope) {
     {name: 'Alfreda Cochran', img: 'img/therapist/freda.jpg', edu: 'LMT'},
     {name: 'Lee McKinney', img: 'img/therapist/lee.jpg', edu: 'LMT'},
     {name: 'Stephen O\'Haire', img: 'img/therapist/stephen.jpg', edu: 'BA, LMT'},
-    {name: 'Misty Humphries', img: 'img/therapist/misty.jpg', edu: 'LMT, MMT'},
     {name: 'Alethea Santas', img: 'img/therapist/alethea.jpg', edu: 'BFA, LMT'},
     {name: 'Allyse Hargrove', img: 'img/therapist/allyse.jpg', edu: 'AS, LMT, CSMT'},
     {name: 'April Milton', img: 'img/therapist/april.jpg', edu: 'Office Manager'},
