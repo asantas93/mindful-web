e851064679f8d323e6ad9dcc65aa51d80f102e49
Aris Santas <asantas@cybraics.com>
2018-02-03 17:16:02 -0500

    Add therapist bios

--- js/aboutController.js
+++ js/aboutController.js
@@ -1,13 +1,75 @@
 function aboutController($scope) {
   $scope.staff = [
-    {name: 'Patricia Santas', img: 'img/therapist/patricia.jpg', edu: 'BA, LMT, Owner'},
-    {name: 'Aristotelis Santas', img: 'img/therapist/ari.jpg', edu: 'PhD, LMT'},
-    {name: 'Alfreda Cochran', img: 'img/therapist/freda.jpg', edu: 'LMT'},
-    {name: 'Lee McKinney', img: 'img/therapist/lee.jpg', edu: 'LMT'},
-    {name: 'Stephen O\'Haire', img: 'img/therapist/stephen.jpg', edu: 'BA, LMT'},
-    {name: 'Alethea Santas', img: 'img/therapist/alethea.jpg', edu: 'BFA, LMT'},
-    {name: 'Allyse Hargrove', img: 'img/therapist/allyse.jpg', edu: 'AS, LMT, CSMT'},
-    {name: 'April Milton', img: 'img/therapist/april.jpg', edu: 'Office Manager'},
-    {name: 'Iain Searcy', img: 'img/therapist/iain.jpg', edu: 'LMT'},
-  ]
+    {
+      name: 'Patricia Santas',
+      img: 'img/therapist/patricia.jpg',
+      edu: 'BA, LMT, Owner',
+      education: ['BA VSU 2000', 'LMT Core Institute 2002'],
+      discounts: ['Teachers', 'Nurses', 'Seniors (70+ years)', 'Military'],
+      bio: 'My approach to massage therapy works with an understanding of western anatomy and physiology and grounded in the eastern tradition of mindfulness. My passion for massage therapy grew out of my personal experience with generalized anxiety and panic attacks. Massage to the scalp created grounding and at thirteen years old I understood at a core level the benefits of touch. I work to facilitate soft tissue balance in the body along with clear mind and open heart.',
+      license: 'MT001891'
+    },
+    {
+      name: 'Aristotelis Santas',
+      img: 'img/therapist/ari.jpg',
+      edu: 'PhD, LMT',
+      education: ['PhD Philosophy Florida State University 1989', 'LMT Core Institute 2002'],
+      discounts: ['VSU faculty, students, and staff', 'Teachers'],
+      bio: 'Massage provides both the client and the therapist focus, balance, and attention to detail in the movement and function of the body. I see massage therapy as an exercise in self-awareness and a movement towards independence in the maintenance of one\'s body-mind. When I work with clients, whether they are athletes in training, or someone seeking relief from chronic pain, I try to give them a sense of what\'s going on both in the specific area needing immediate attention and in the body as a dynamic whole. That awareness is the first step towards relaxation and relief, and in many cases self-care and independence. I emphasize the dynamic of body-mind in muscular tension--how thinking manifests in muscular contraction and repetitive thoughts without attendant action creates tension and somatic dysfunction. I always work to help my clients understand what I find going on in their bodies, and provide exercises for those clients who wish to release trapped tension by participating in their own healing and relaxation.',
+      license: 'MT002654'
+    },
+    {
+      name: 'Alfreda Cochran',
+      img: 'img/therapist/freda.jpg',
+      edu: 'LMT',
+      license: 'MT001470'
+    },
+    {
+      name: 'Lee McKinney',
+      img: 'img/therapist/lee.jpg',
+      edu: 'LMT',
+      education: ['LMT Florida School of Massage 2009'],
+      discounts: ['Teachers', 'Nurses', 'Seniors (70+ years)', 'Military', 'Students', 'Animal organization volunteers/fosters'],
+      bio: 'Massage Therapy called to me for its healing and energetic benefits. I strive to offer my clients a safe and nonjudgmental place to unwind the mind and body from the daily stressors of life.',
+      license: 'MT007158'
+    },
+    {
+      name: 'Stephen O\'Haire',
+      img: 'img/therapist/stephen.jpg',
+      edu: 'BA, LMT',
+      education: ['LMT Florida School of Massage 2008', 'BA Exercise Physiology VSU 2005'],
+      discounts: ['Students', 'Teachers'],
+      bio: 'I found interest in massage therapy after an auto accident in 2004 left me with a serious lower back injury. I found that massage not only aleviated my back pain, but also left me with a general sense of happiness and ease long after receiving the bodywork. I try to pass the same benefits on to each client I see. I believe massage is a tool that can be used to promote numerous health benefits as well as universally elevate the overall quality of life of it\'s recipients.',
+      license: 'MT005597'
+    },
+    {
+      name: 'Alethea Santas',
+      img: 'img/therapist/alethea.jpg',
+      edu: 'BFA, LMT',
+      education: ['BFA Valdosta State University 2003', 'Master Cosmetologist Paul Mitchell the School 2004', 'LMT Florida School of Massage 2015'],
+      discounts: ['Students', 'Teachers', 'Seniors (70+ years)', 'Disabled/Handicapped', 'Nurses'],
+      bio: 'As a body-worker, I approach each client from both a broad and focused perspective, treating at least the symptomatic area and directly related muscle groups.  Given enough time, I also address as much of the whole body as possible.  The soft tissue system is extensive and often what is felt by the person as an ache or pain is only one manifestation of a network pattern. I believe it is important to acknowledge that a person is more than tissue, but also has a mind and an essence that influence their physical properties. This is why I pay attention to the expression of each individual, noticing and working with things like breath, and emotion when relevant. My goal is to help relieve and relax each client according to their needs and hopefully bring awareness to the root of any issues. If pertinent, I offer self care practices one may take home to help manage pain, deconstruct negative patterns, and bring balance. I encourage everyone to be active in his or her own life finding ways to be healthy and happy. It brings me much pleasure to be a part of peoples\' paths of healing and self-discovery!',
+      license: 'MT010934'
+    },
+    {
+      name: 'Allyse Hargrove',
+      img: 'img/therapist/allyse.jpg',
+      edu: 'AS, LMT, CSMT',
+      education: ['Diploma of Neuromuscular Massage Therapy Southern Regional Technical College 2016', 'AS Secondary Education Georgia Military College 2010'],
+      discounts: ['Military', 'Young athletes'],
+      bio: 'I became a massage therapist to help people achieve relief whether it be from muscular tension, stress, or chronic or acute injuries, as well as promote healing and performance, amongst athletes. I hope to provide my clients with the tools and knowledge to continue their therapy during their everyday lives.',
+      license: 'MT011249'
+    },
+    {
+      name: 'April Milton',
+      img: 'img/therapist/april.jpg',
+      edu: 'Office Manager'
+    },
+    {
+      name: 'Iain Searcy',
+      img: 'img/therapist/iain.jpg',
+      edu: 'LMT',
+      license: 'MT011757'
+    },
+  ];
 }
