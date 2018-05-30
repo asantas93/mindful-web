cccb3eba977e9dff1a4ee5f355d0c65f0ac6a1e2
Aris Santas <asantas@cybraics.com>
2018-05-29 20:52:05 -0400

    Update discounts

--- js/aboutController.js
+++ js/aboutController.js
@@ -5,6 +5,7 @@ function aboutController($scope) {
       img: 'img/therapist/patricia.jpg',
       edu: 'BA, LMT, Owner',
       education: ['BA VSU 2000', 'LMT Core Institute 2002'],
+      discount: '$10',
       discounts: ['Teachers', 'Nurses', 'Seniors (70+ years)', 'Military'],
       bio: 'My approach to massage therapy works with an understanding of western anatomy and physiology and grounded in the eastern tradition of mindfulness. My passion for massage therapy grew out of my personal experience with generalized anxiety and panic attacks. Massage to the scalp created grounding and at thirteen years old I understood at a core level the benefits of touch. I work to facilitate soft tissue balance in the body along with clear mind and open heart.',
       license: 'MT001891'
@@ -14,6 +15,7 @@ function aboutController($scope) {
       img: 'img/therapist/ari.jpg',
       edu: 'PhD, LMT',
       education: ['PhD Philosophy Florida State University 1989', 'LMT Core Institute 2002'],
+      discount: '$10',
       discounts: ['VSU faculty, students, and staff', 'Teachers'],
       bio: 'Massage provides both the client and the therapist focus, balance, and attention to detail in the movement and function of the body. I see massage therapy as an exercise in self-awareness and a movement towards independence in the maintenance of one\'s body-mind. When I work with clients, whether they are athletes in training, or someone seeking relief from chronic pain, I try to give them a sense of what\'s going on both in the specific area needing immediate attention and in the body as a dynamic whole. That awareness is the first step towards relaxation and relief, and in many cases self-care and independence. I emphasize the dynamic of body-mind in muscular tension--how thinking manifests in muscular contraction and repetitive thoughts without attendant action creates tension and somatic dysfunction. I always work to help my clients understand what I find going on in their bodies, and provide exercises for those clients who wish to release trapped tension by participating in their own healing and relaxation.',
       license: 'MT002654'
@@ -29,7 +31,6 @@ function aboutController($scope) {
       img: 'img/therapist/lee.jpg',
       edu: 'LMT',
       education: ['LMT Florida School of Massage 2009'],
-      discounts: ['Teachers', 'Nurses', 'Seniors (70+ years)', 'Military', 'Students', 'Animal organization volunteers/fosters'],
       bio: 'Massage Therapy called to me for its healing and energetic benefits. I strive to offer my clients a safe and nonjudgmental place to unwind the mind and body from the daily stressors of life.',
       license: 'MT007158'
     },
@@ -38,7 +39,6 @@ function aboutController($scope) {
       img: 'img/therapist/stephen.jpg',
       edu: 'BA, LMT',
       education: ['LMT Florida School of Massage 2008', 'BA Exercise Physiology VSU 2015'],
-      discounts: ['Students', 'Teachers'],
       bio: 'I found interest in massage therapy after an auto accident in 2004 left me with a serious lower back injury. I found that massage not only aleviated my back pain, but also left me with a general sense of happiness and ease long after receiving the bodywork. I try to pass the same benefits on to each client I see. I believe massage is a tool that can be used to promote numerous health benefits as well as universally elevate the overall quality of life of it\'s recipients.',
       license: 'MT005597'
     },
@@ -49,6 +49,7 @@ function aboutController($scope) {
       mmfl: true,
       edu: 'BFA, LMT',
       education: ['BFA Valdosta State University 2003', 'Master Cosmetologist Paul Mitchell the School 2004', 'LMT Florida School of Massage 2015'],
+      discount: '$10',
       discounts: ['Students', 'Teachers', 'Seniors (70+ years)', 'Disabled/Handicapped', 'Nurses'],
       bio: 'As a body-worker, I approach each client from both a broad and focused perspective, treating at least the symptomatic area and directly related muscle groups.  Given enough time, I also address as much of the whole body as possible.  The soft tissue system is extensive and often what is felt by the person as an ache or pain is only one manifestation of a network pattern. I believe it is important to acknowledge that a person is more than tissue, but also has a mind and an essence that influence their physical properties. This is why I pay attention to the expression of each individual, noticing and working with things like breath, and emotion when relevant. My goal is to help relieve and relax each client according to their needs and hopefully bring awareness to the root of any issues. If pertinent, I offer self care practices one may take home to help manage pain, deconstruct negative patterns, and bring balance. I encourage everyone to be active in his or her own life finding ways to be healthy and happy. It brings me much pleasure to be a part of peoples\' paths of healing and self-discovery!',
       license: 'MT010934'
@@ -58,7 +59,8 @@ function aboutController($scope) {
       img: 'img/therapist/allyse.jpg',
       edu: 'AS, LMT, CSMT',
       education: ['Diploma of Neuromuscular Massage Therapy Southern Regional Technical College 2016', 'AS Secondary Education Georgia Military College 2010'],
-      discounts: ['Military', 'Young athletes'],
+      discount: '$5',
+      discounts: ['Military', 'CrossFit'],
       bio: 'I became a massage therapist to help people achieve relief whether it be from muscular tension, stress, or chronic or acute injuries, as well as promote healing and performance, amongst athletes. I hope to provide my clients with the tools and knowledge to continue their therapy during their everyday lives.',
       license: 'MT011249'
     },
@@ -74,6 +76,7 @@ function aboutController($scope) {
       edu: 'LMT',
       mmfl: true,
       education: ['LMT Florida School of Massage 2015'],
+      discount: '$10',
       discounts: ['Teachers'],
       bio: 'I love exploring how the layers of muscles coincide and relate to the overall issue. I work with breathing and awareness of the pressure points. I find this speeds up the progress of a session. Growing up with therapist parents and friends gave me a lot of experience receiving different kinds of work throughout the years. This helped me find what I liked in a good massage and how to incorporate it into my work. I love what I do and hope you will too.',
       license: 'MT011757'
@@ -83,17 +86,10 @@ function aboutController($scope) {
       img: 'img/therapist/patriciah.jpg',
       edu: 'MS, LMT',
       education: ['MS Counseling Long Island University 2001', 'LMT Pacific College of Oriental Medicine'],
-      discounts: ['Seniors'],
+      discount: '10%',
+      discounts: ['Seniors', 'Military'],
       bio: 'My endeavor is to give each client a comforting and soothing experience that will enhance their mental and physical well being. One that aids in helping them improve their quality of life by providing relief from physical discomfort and everyday stress. My overall goal within the field is to continue growing, continue learning and to continue the joy of giving, into the world of massage just as much as those who enjoy the receiving. I want to extend a warm thank you to each and everyone that place their trust into my ability and expertise.',
       license: 'MT007387'
-    },
-    {
-      name: 'Brey Littleton',
-      img: 'img/therapist/brey.jpg',
-      edu: 'LMT',
-      education: ['LMT Center for Massage and Natural Health 2017'],
-      discounts: ['Students', 'Teachers'],
-      bio: 'As a massage and bodywork therapist I want to create an experience for people that will allow them to sink more deeply into the present and begin to understand health by listening to the body itself. By offering a service that promotes a better relationship to form between mind and body I hope to act as a bridge for people to help them connect to their own sense of balance. I want to be able to help people become more aware of their \'vital energy\' so they can access more of their potential and experience greater wholeness and well-being.',
     },
   ];
 }
