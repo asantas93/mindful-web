22f9371843de51283544925c8d8197ecec682961
Aris Santas <asantas93@gmail.com>
2020-03-16 16:58:49 -0400

    Add S3 sync script

---
+++ s3-synch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eou pipefail
+
+export AWS_PROFILE=mindful-api
+export BUCKET=s3://mindfulmassage.biz
+aws s3 sync --acl public-read --cache-control max-age=3600 js $BUCKET/js
+aws s3 sync --acl public-read --cache-control max-age=3600 html $BUCKET/html
+aws s3 sync --acl public-read --cache-control max-age=3600 css $BUCKET/css
+aws s3 sync --acl public-read --cache-control max-age=1209600 img $BUCKET/img
+aws s3 sync --acl public-read --cache-control max-age=1209600 favicons $BUCKET/favicons
+aws s3 cp --acl public-read --cache-control max-age=3600 index.html $BUCKET/index.html
