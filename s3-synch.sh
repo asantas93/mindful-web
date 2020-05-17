3237f77c0dd971d2b5e50aae0d6d669416c008de
Aris Santas <asantas93@gmail.com>
2020-05-17 19:26:56 -0400

    add files to sync

--- s3-synch.sh
+++ s3-synch.sh
@@ -5,6 +5,7 @@ set -eou pipefail
 export AWS_PROFILE=mindful-api
 export BUCKET=s3://mindfulmassage.biz
 aws s3 sync --acl public-read --cache-control max-age=3600 js $BUCKET/js
+aws s3 sync --acl public-read --cache-control max-age=3600 files $BUCKET/files
 aws s3 sync --acl public-read --cache-control max-age=3600 html $BUCKET/html
 aws s3 sync --acl public-read --cache-control max-age=3600 css $BUCKET/css
 aws s3 sync --acl public-read --cache-control max-age=1209600 img $BUCKET/img
